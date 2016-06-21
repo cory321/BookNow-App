b9e7fb455a1ed615fbc81abb6e21d85af788e9e4
cory321 <cory321@gmail.com>
2016-06-21 10:14:13 -0700

    Added Avatar Upload to Assistants

--- bookNowApp/app/models/assistant.rb
+++ bookNowApp/app/models/assistant.rb
@@ -1,2 +1,11 @@
 class Assistant < ActiveRecord::Base
+	mount_uploader :avatar, AvatarUploader
+	validate :image_size_validation
+
+	# build functionality - dependent destroy uploaded avatar
+ 
+	private
+	  def image_size_validation
+	    errors[:avatar] << "should be less than 500KB" if avatar.size > 0.5.megabytes
+	  end
 end

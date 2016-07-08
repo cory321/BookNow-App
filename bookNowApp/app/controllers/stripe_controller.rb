302f5e59dfb426e46734287018e4c892bb5a751d
cory321 <cory321@gmail.com>
2016-07-07 17:05:31 -0700

    Stripe Controller now persists data

--- bookNowApp/app/controllers/stripe_controller.rb
+++ bookNowApp/app/controllers/stripe_controller.rb
@@ -1,20 +1,21 @@
 class StripeController < ApplicationController
+	before_action :confirm_logged_in
 
 	def index
 
-		 # binding.pry
-		
 	end
 
 	def authorize
 		params = {
 		     :scope => 'read_write'
 		     #pre-fill user's info (optional)
-		     # 'stripe_user[email]' => 'me@bettycrocker.com',
-		     # 'stripe_user[url]' => 'https://dessertmarket.com/betty-crocker',
-		     # 'stripe_user[first_name]' => 'Betty',
-		     # 'stripe_user[last_name]' => 'Crocker'
+		     # 'stripe_user[email]' => current_user.email,
+		     # 'stripe_user[url]' => current_user.website,
+		     # 'stripe_user[first_name]' => current_user.first_name,
+		     # 'stripe_user[last_name]' => current_user.last_name
 		   }
+
+
 
     # Redirect the user to the authorize_uri endpoint
     redirect_to client.auth_code.authorize_url(params)
@@ -30,7 +31,7 @@ class StripeController < ApplicationController
 			render :index
 			
 		else
-			
+
 			# Pull the authorization_code out of the response
 			code = params[:code]
 
@@ -38,16 +39,24 @@ class StripeController < ApplicationController
 		  @response = client.auth_code.get_token(code, :params => {:scope => 'read_write'})
 		  
 		  # Use the access_token as you would a regular live-mode API key
+		  @refresh_token = @response.refresh_token
 		  @access_token = @response.token
 
-		  render :index
+			current_user.update(stripe_publishable_key: @response.params["stripe_publishable_key"], stripe_user_id: @response.params["stripe_user_id"], stripe_refresh_token: @refresh_token, stripe_access_token: @access_token)
+		  if current_user.save
+		    redirect_to stripe_path, flash: {success: "Yes it worked!"}
+		  else
+		    redirect_to stripe_path, flash: {error: "It Failed"}
+		  end
 
 		  # @response.params["stripe_user_id"]
+		  # @response.params["stripe_publishable_key"]
+		  # @refresh_token
+		  # @access_token
+
 		  # TODO: Stripe logic
 		end
 	end
-
-
 
 	private
 	
@@ -63,6 +72,5 @@ class StripeController < ApplicationController
 	def client
 		@client ||= OAuth2::Client.new(ENV['STRIPE_CONNECT_CLIENT_ID'], ENV['STRIPE_SECRET_KEY'], options)
 	end
-
 
 end
